600df25c0eb6079d4fd09214e134485313e9b669
Marcelo Martins <marcelo-martins@defensoria.rs.def.br>
2023-08-27 01:14:25 -0300

    feat: larger buffer and ability to move screen

--- game.c
+++ game.c
@@ -8,6 +8,10 @@
 #define DEAD ' '
 #define SPACE 32
 #define ESC 27
+#define A 97
+#define D 100
+#define W 119
+#define S 115
 
 int count_neighbors(int x, int y, char *buffer, int width, int height) {
   int count = 0;
@@ -57,12 +61,15 @@ int main(int argc, char *argv[]) {
 
   initscr();
   noecho();
-  timeout(100);
+  timeout(0);
 
   int input = 0;
-  int row_size = w.ws_row - 1;
-  int col_size = w.ws_col;
-  int buffer_size = w.ws_col * w.ws_row;
+  int col_offset = 0;
+  int row_offset = 0;
+  // TODO: Make these configurable by command line arguments
+  int row_size = 1000;
+  int col_size = 1000;
+  int buffer_size = row_size * col_size;
 
   char *buffer = malloc(buffer_size * sizeof(char));
   char *nbuffer = malloc(buffer_size * sizeof(char));
@@ -74,6 +81,14 @@ int main(int argc, char *argv[]) {
 
     if (input == SPACE) {
       randomize_buffer(buffer, nbuffer, row_size, col_size);
+    } else if (input == A) {
+      col_offset--;
+    } else if (input == D) {
+      col_offset++;
+    } else if (input == W) {
+      row_offset--;
+    } else if (input == S) {
+      row_offset++;
     }
 
     for (int i = 0; i < row_size; i++) {
@@ -92,6 +107,25 @@ int main(int argc, char *argv[]) {
         } else {
           nbuffer[current] = count == 3 ? ALIVE : DEAD;
         }
+      }
+    }
+
+    ioctl(0, TIOCGWINSZ, &w);
+    if (col_offset < 0) {
+      col_offset = 0;
+    } else if (col_offset > (col_size - w.ws_col)) {
+      col_offset = col_size - w.ws_col;
+    }
+
+    if (row_offset < 0) {
+      row_offset = 0;
+    } else if (row_offset > (row_size - w.ws_row)) {
+      row_offset = row_size - w.ws_row;
+    }
+
+    for (int i = 0; i < w.ws_row; i++) {
+      for (int j = 0; j < w.ws_col; j++) {
+        int current = (i + row_offset) * row_size + (j + col_offset);
         mvprintw(i, j, "%c", nbuffer[current]);
       }
     }
